63dcceb7fa1c308f94575fbf7a1654db9c874341
Júlia Prado Bezerra <juliabezerraaa@gmail.com>
2024-11-19 11:35:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+﻿criaCartao(
+    'Alimentação',
+    'Ração de cachorro faz bem para pessoas?',
+    'Possivelmente não'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Japão?',
+    'A capital do Japão é Tóquio'
+)
+
+criaCartao(
+    'Historia',
+    'Como foi chamado a ditadura de Vargas?',
+    'Estado Novo'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz banco na Língua Inglesa?',
+    'Banco se diz Bench'
+)
